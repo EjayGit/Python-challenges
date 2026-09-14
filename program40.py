a760530a3088a0a9761de684aa9753582a1e94ec
EjayGit <erikjclark@icloud.com>
2026-09-14 15:23:40 +0100

    commit program

--- program40.py
+++ program40.py
@@ -1,2 +1,11 @@
 # Sort words in alphabetical order.
 
+string = input("Enter a string: ").lower()
+
+wordsList = string.split()
+
+sortedWords = sorted(wordsList)
+
+print(f'The words in order are: ')
+for word in sortedWords:
+    print(word)
