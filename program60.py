5212e86347e1101c86315a467975f87c54e26de1
EjayGit <erikjclark@icloud.com>
2026-09-16 17:03:41 +0100

    commit program

--- program60.py
+++ program60.py
@@ -4,8 +4,7 @@ string = 'HellOo Universe'
 characterNum = 5
 
 def removeChar(string, character):
-    newString = string[:character-1]
-    newString = newString + string[character:]
+    newString = string[:character-1] + string[character:]
     return newString
 
 print(f'The new string is: {removeChar(string, characterNum)}.')
